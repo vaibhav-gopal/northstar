4f0fcb8abe2bf883500bbe688af836e98f8b2393
vaibhav-gopal <vabsgop@gmail.com>
2024-02-04 13:24:14 -0500

    Gave up

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -6,30 +6,19 @@ import { redirect } from 'next/navigation'
 
 export default function Dashboard() {
     const { user, error, isLoading } = useUser();
-    const [goalList, setGoalList] = useState(undefined);
-
-    const getGoalList = async () => {
-        const res = await fetch(`/api/getGoalList?name=${user.name}`);
-        const body = await res.json();
-        setGoalList(body);
-    }
-
-    useEffect(() => {
-        if (!user) {
-            alert("Please login first");
-            redirect('/');
-        }
-
-        getGoalList();
-    }, [user]);
+    const [goalList, setGoalList] = useState({
+        "Get Shredded": false,
+        "Get Rich": false,
+        "Get Diabetes": true
+    });
 
     return (
         user && (
             <div className="flex mx-auto min-h-screen flex-col items-center justify-start px-14 max-w-6xl">
                 <h2 className= "text-4xl font-bold py-14 text-[#F1F1F1]">Welcome back, {user.name}!</h2>
-                {goalList &&
-                    goalList.map((item, i) => {
-                        <>{item} : {i}</>
+                {
+                    Object.keys(goalList).map((item, i) => {
+                        <h1 key={i}>{i} : {goalList[i]}</h1>
                     })
                 }
             </div>
