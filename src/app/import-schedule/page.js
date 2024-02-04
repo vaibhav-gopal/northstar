24de3b43f55c17812dfaf2d470aabfeb91d57d7b
Cheukman Zhou <cheukmanzhou@gmail.com>
2024-02-03 21:20:17 -0500

    Added import-schedule page, bare bones

---
+++ src/app/import-schedule/page.js
@@ -0,0 +1,14 @@
+import Button from '../ui/button.js';
+
+export default function ScheduleImport() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="">
+                <p className=''>
+                    Import Schedule
+                </p>
+                <Button link="" text="IMPORT"></Button>
+            </div>
+        </main>
+    );
+}
