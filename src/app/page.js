3fa988718bcbd812ad29801757c0625103f9f48b
vaibhav-gopal <vabsgop@gmail.com>
2024-02-03 19:49:08 -0500

    Implemented Button component for visualization

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
+import Button from './ui/button.js';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <Button></Button>
     </main>
   );
 }
