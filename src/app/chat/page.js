0c9c0268fad763c6d516cee35fd3a8025f83ce66
Hashim B <119533161+HashBukhtiar@users.noreply.github.com>
2024-02-04 11:00:32 -0500

    Added some code for input field in page.js

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,8 +1,18 @@
+import React from 'react';
 
 export default function Chat() {
+    const inputStyles = {
+        color: 'black',
+        border: '1px solid black',
+        padding: '5px',
+        fontFamily: 'font-sans', // Specify the desired font family
+    };
+
     return (
         <div className="flex mx-auto min-h-screen flex-col items-center justify-start px-14 max-w-6xl">
-
+            <label htmlFor="textInput">Enter Text:</label>
+            <input type="text" id="textInput" name="textInput" placeholder="Type here..." style={inputStyles} />
         </div>
     );
 }
+
