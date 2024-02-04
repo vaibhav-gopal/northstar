bc1456a728cfbd103e20a0d9728390999c823b72
vaibhav-gopal <vabsgop@gmail.com>
2024-02-03 23:33:16 -0500

    Added auth api route

---
+++ src/app/api/auth/route.js
@@ -0,0 +1,4 @@
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+export function GET(request) {
+    return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+}
